3c458373fc80d5d4bda6172a523042a4ff6ec382
Wendell Bitencourt <bat.delgeorge@hotmail.com>
2025-11-07 14:05:05 -0300

    update

--- node-aws/src/lib/db-dynamodb.ts
+++
@@ -1,8 +0,0 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
-
-const client = new DynamoDBClient({
-  region: process.env.AWS_REGION, 
-});
-
-export const dynamoClient = DynamoDBDocumentClient.from(client);
